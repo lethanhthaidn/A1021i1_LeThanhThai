7fb507c86c1f84a79ea69421ed3165d19903c80b
lethanhthaidn <lethaidn92@gmail.com>
2022-02-20 20:47:11 +0700

    exception

---
+++ src/ss15_Exception/Bai_tap/IllegalTriangleException.java
@@ -0,0 +1,5 @@
+package ss15_Exception.Bai_tap;
+
+public class IllegalTriangleException extends Exception {
+    public IllegalTriangleException(String message) {super(message);}
+}
--- src/ss15_Exception/Bai_tap/TriangleException.java
+++ src/ss15_Exception/Bai_tap/TriangleException.java
@@ -3,7 +3,9 @@ package ss15_Exception.Bai_tap;
 import java.util.Scanner;
 
 public class TriangleException {
-    public static <IllegalTriangleException> void main(String[] args) {
+    public static void main(String[] args) {
+
+
         int a, b, c;
         Scanner scanner = new Scanner(System.in);
         while (true) {
@@ -14,15 +16,28 @@ public class TriangleException {
             System.out.print("Cạnh c: ");
             c = Integer.parseInt(scanner.nextLine());
             int perimeter = a + b + c;
+//        checkTriangle(a,b,c);
 
             try {
                 if (a < 0 || b < 0 || c < 0 || a + b < c || a + c < b || b + c < a) {
-                    throw new Exception("Bạn đã nhập số âm, mời bạn nhập lại.");
+                    throw new IllegalTriangleException("Bạn đã nhập số âm, mời bạn nhập lại.");
                 } else
-                    System.out.print("Perimeter: " + perimeter);break;
-            } catch (Exception e) {
+                    System.out.print("Perimeter: " + perimeter);
+                break;
+            } catch (IllegalTriangleException e) {
                 System.out.println(e.getMessage());
             }
         }
     }
+
+//    private static void checkTriangle(int a, int b, int c) {
+//        try {
+//            if (a < 0 || b < 0 || c < 0 || a + b < c || a + c < b || b + c < a) {
+//                throw new Exception("Bạn đã nhập số âm, mời bạn nhập lại.");
+//            } else
+//                System.out.print("Perimeter: " + a + b + c);
+//        } catch (Exception e) {
+//            System.out.println(e.getMessage());
+//        }
+//    }
 }
